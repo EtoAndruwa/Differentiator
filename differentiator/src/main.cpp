33bad3b874bb3d16149a33c05eb13d11ee3fa944
EtoAndruwa <timonina1909@gmail.com>
2023-03-28 05:14:14 +0300

    Up-to-date files

--- differentiator/src/main.cpp
+++ differentiator/src/main.cpp
@@ -5,8 +5,10 @@ int main()
     Tree* tree_ptr = tree_ctor();
 
     Node* node_1 = create_node(tree_ptr, 9);
-    Node* node_2 = create_node(tree_ptr, 2.1);
-    tree_ptr->root = create_node(tree_ptr, ADD, IS_OP, node_1, node_2);
+    Node* node_2 = create_node(tree_ptr, 100);
+    Node* node_4 = create_node(tree_ptr, 3);
+    Node* node_3 = create_node(tree_ptr, MUL, IS_OP, node_1, node_2);
+    tree_ptr->root = create_node(tree_ptr, ADD, IS_OP, node_3, node_4);
     printf("\nvalue: %f\n", eval(tree_ptr->root));
     generate_cpu_code(tree_ptr->root);
     // print_leaves(tree_ptr->root);
